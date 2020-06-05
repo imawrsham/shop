1d6b3b059c49cf5fa443b8a16963e92ddbf487fb
Arsham <arsham.asgarizadeh@gmail.com>
2020-06-05 10:33:15 +0200

    Header Footer Menu

---
+++ view/header.php
@@ -0,0 +1,14 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <div class="dt-loader-container">
+            <div class="ts-wrapper">
+                <div class="loader-text text-center mb-3"><img src="images/icon-vector.png" class="img-fluid" style="height: 100px"></div>
+                <div class="tsc-loader">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "menu.html";?>
+</nav>
