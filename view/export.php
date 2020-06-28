4efa81a25e3a253584aebc56618a3197ea0a27db
Atiyeh Rezaei <atiyehrezaei12@gmail.com>
2020-06-28 22:44:58 +0200

    modified export file

--- view/export.php
+++ view/export.php
@@ -12,22 +12,26 @@ if(isset($_SESSION['username'])){
     $id = $_SESSION['id'];
     //var_dump($id);
     $allData ="";
-    $sql = "SELECT * FROM userorder_items";
-    $result = mysqli_query($conn, $sql);
+    //$sql = "SELECT * FROM userorder_items";
+    //$result = mysqli_query($conn, $sql);
     //var_dump($sql);
-    //$sql1 = "SELECT id FROM userorders WHERE id='" . $id . "'";
-   // $result2 = mysqli_query($conn, $sql1);
+    $sql1 = "SELECT id FROM userorders WHERE userid='" . $id . "'";
+    $result2 = mysqli_query($conn, $sql1);
     //$row2 = mysqli_fetch_assoc($result2);
     //var_dump($sql1);
-    //$sql = "SELECT * FROM userorder_items where userorderid= '".$row2['id']."'";
+    //var_dump($result2);
+    while ($row2 = mysqli_fetch_assoc($result2)) {
+    $sql = "SELECT * FROM userorder_items where userorderid= '".$row2['id']."'";
     //var_dump($sql);
-    //$result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
+   // var_dump($result2);
     while($data = mysqli_fetch_assoc($result)) {
-        $allData .= $data['productname'].','.$data['productprice']."\n";
-        $response = "data:text/csv;charset=utf-8,Name,Price\n";
+        $allData .=$data['userorderid'].',' .$data['productname'].','.$data['productprice']."\n";
+        $response = "data:text/csv;charset=utf-8,Orderid,Name,Price\n";
         $response .=$allData;
-
-    }
-    echo '<a href="'.$response.'" download="export.csv">Download</a>';
+        //echo '<a href="'.$response.'" download="export.csv">Download</a>';
+    }}
+    echo '<a href="'.$response.'" download="export.csv">Download</a><br/>';
+    echo "<a href='homeuser.php'>back</a><br/><br/>";
 }
     ?>
